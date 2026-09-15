85135149004b4862f8f6c3b4a47be39f30a2eed9
Your Name <mohitkumr17082004@gmail.com>
2026-09-15 10:21:34 +0530

    second commits

--- my_app_infra_module/my-bucket.tf
+++ my_app_infra_module/my-bucket.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "my_app_bucket" {
-  bucket = "${var.my_env}-tws-demo-app-bucket"
+  bucket = "${var.my_env}-tws-demo-app-bucket-61972683002"
 
   tags = {
-    Name = "${var.my_env}-tws-demo-app-bucket"
+    Name = "${var.my_env}-tws-demo-app-bucket-61972683002"
   }
 }
 
